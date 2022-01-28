a69ec255d504001ed2621b291cd692f2c267f1ca
Samiyamahad <90895899+Samiyamahad@users.noreply.github.com>
2022-01-28 15:25:52 +0300

    Add files via upload

---
+++ Intro_p5.js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,220,0,600);
+  fill( 00,)
+  ellipse(56, 46, 55, 55);
+   fill(175, 100, 220);
+  square(250, 200, 55, 20, 15, 10, 5);
+ 
+}
