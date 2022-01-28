121f2f317d75bafbc1d5f86b6650720f9fe913b0
Samiyamahad <90895899+Samiyamahad@users.noreply.github.com>
2022-01-28 15:51:36 +0300

    Add files via upload

---
+++ printing name/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function person(){
+  var frist_Name=" Samiya "
+  var last_Name="mahad"
+  console.log(frist_Name+ last_Name)
+}
+person();
