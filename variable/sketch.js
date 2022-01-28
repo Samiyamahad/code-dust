c206d530714dd17823ea816d6bcd65985f1113ec
Samiyamahad <90895899+Samiyamahad@users.noreply.github.com>
2022-01-28 15:40:18 +0300

    Add files via upload

---
+++ variable/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 300);
+  background(0)
+}
+
+function draw() {
+ 
+  
+  noStroke();
+  fill(255, 204, 0);
+  circle(mouseX,mouseY,24);
+  
+}
+
+function mousePressed(){
+  background(0);
+}
